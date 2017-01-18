ca36449a90b48e68b4bcd9ec606fa2c2556e527f
Swathy Jayaseelan <swathyjayaseelan@gmail.com>
2017-01-18 10:08:24 -0800

    Project files

---
+++ js/main.js
@@ -0,0 +1,181 @@
+var map;
+var uniqueId = 1;
+var markers = [];
+//Map initialisation with default markers
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 33.6694649,
+            lng: -117.8231107
+        },
+        zoom: 13
+    });
+    window.infowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+    var service = new google.maps.places.PlacesService(map);
+    //Markers for all the default locations
+    for (var i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var ID = locations[i].yelpID;
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: title,
+            yelpid: ID
+        });
+        bounds.extend(marker.position);
+        marker.id = uniqueId;
+        markers.push(marker);
+        marker.addListener('click', function() {
+            this.setAnimation(google.maps.Animation.BOUNCE);
+            displayinfoWindow(this, infowindow);
+
+        });
+        uniqueId++;
+    }
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+    map.fitBounds(bounds);
+}
+//Display info window when a marker is clicked
+function displayinfoWindow(marker, infowindow) {
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '<div');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick', function() {
+            infowindow.setMarker(null);
+        });
+    }
+}
+//To clear markers on map to display only the markers of locations that are selected
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+//Display details when a location from the list is clicked and bounce the respective marker
+function displayDetails(clickedLocation) {
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].title === clickedLocation.title) {
+            markers[i].setAnimation(google.maps.Animation.BOUNCE);
+            //console.log(clickedLocation.yelpID);
+            getyelpData(clickedLocation.yelpID);
+        }
+    }
+}
+//ViewModel to link the data with UI
+var ViewModel = function() {
+    var self = this;
+    self.location = ko.observableArray(locations);
+    self.locationList = ko.observableArray([]);
+    locations.forEach(function(data) {
+        self.locationList.push(data);
+    });
+    self.details = ko.observable("");
+    self.shouldShow = ko.observable("true");
+    self.displayLocation = function(clickedLocation) {
+        displayDetails(clickedLocation);
+    };
+    self.displaySpecificLocation = function(clickedLocation) {
+        displayspecificMarker(clickedLocation);
+    };
+    self.setUp = function() {
+        if ($("#myDropdown").hasClass("show")) {
+            setMapOnAll(null);
+            vm.locationList([]);
+        }
+    };
+};
+var vm = new ViewModel();
+ko.applyBindings(vm);
+//to refresh the map and list whenever refresh button is clikced
+function refresh() {
+    vm.locationList([]);
+    locations.forEach(function(data) {
+        vm.locationList.push(data);
+    });
+    vm.details("");
+    initMap();
+}
+//to open the menu bar in mobile browsers
+function myopen() {
+    if ($("#sidebar").hasClass("menu")) {
+        $("#sidebar").removeClass("menu");
+        $("#sidebar").addClass("openbar");
+    } else {
+      $("#sidebar").removeClass("openbar");
+        $("#sidebar").addClass("menu");
+    }
+}
+//Function to handle the drop down menu, display the content and hide the existing list and hide the content and
+//display the existing list
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    if ($("#myDropdown").hasClass("show")) {
+        vm.shouldShow(false);
+    }
+}
+//function to display the marker when a location from the drop down menu is clicked
+function displayspecificMarker(clickedLocation) {
+    var marker = new google.maps.Marker({
+        position: clickedLocation.location,
+        map: map,
+        title: clickedLocation.title,
+    });
+    marker.addListener('click', function() {
+        displayinfoWindow(this, infowindow);
+    });
+    markers.push(marker);
+    vm.shouldShow(true);
+    vm.locationList.push(clickedLocation);
+}
+function nonce_generate() {
+    return (Math.floor(Math.random() * 1e12).toString());
+}
+//to connect to yelp API and display details
+function getyelpData(yelpid) {
+    console.log("hi");
+    var yelpconsumerKey = 'Gd8UyTzfV-tWPSXws41oHw';
+    var yelpconsumerSecret = 'UMIW-d64I_5-bmE40K_fTwhV8PU';
+    var yelpToken = "A4b1JoCwcFuUGkfBTbhtn3biy76AFUlH";
+    var yelptokenSecret = "fZh6o9yxyfgVAbrHJCiXEaM0D90";
+    var yelpUrl = "https://api.yelp.com/v2/business/" + yelpid;
+    var parameters = {
+        oauth_consumer_key: yelpconsumerKey,
+        oauth_token: yelpToken,
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now() / 1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0',
+
+        callback: 'cb'
+    };
+    var encodedSignature = oauthSignature.generate('GET', yelpUrl, parameters, yelpconsumerSecret, yelptokenSecret);
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+        url: yelpUrl,
+        data: parameters,
+        cache: true,
+        dataType: 'jsonp',
+        success: function(results) {
+            console.log(results.location.address);
+            vm.details('<h4>' + results.name + '</h4><br>' +
+                '<img src="' + results.rating_img_url + '" alt="Rating"><br>' +
+                '<img src="' + results.image_url + '" alt="' + results.name + '"><br>' +
+                '<p>Address: ' + results.location.address + ' </p>' +
+                '<p>Tel:' + results.display_phone + ' </p>' +
+                '<p>Link: <a href ="' + results.url + '">Open in Yelp</a></p>' +
+                '<p>*From www.yelp.com*</p>');
+        },
+        error: function() {
+            alert("Sorry ! an error has occured. Please refresh and try again");
+        }
+    };
+    $.ajax(settings);
+}
---
+++ js/model.js
@@ -0,0 +1,38 @@
+var locations = [
+  {
+    title: "Irvine Spectrum",
+    location: {lat: 33.6533, lng: -117.7525},
+    yelpID: "irvine-spectrum-center-irvine-10"
+  },
+  {
+    title: "Huntington beach",
+    location: {lat:  33.660057, lng: -117.998970},
+    yelpID: "huntington-beach-huntington-beach"
+  },
+  {
+    title: "Dana Point",
+    location: {lat: 33.4669721 , lng: -117.69810749999999},
+    yelpID: "dana-point-harbor-dana-point-2"
+  },
+  {
+    title: "New Port Beach",
+    location: {lat: 33.6189101 , lng: -117.9289469},
+    yelpID: "newport-beach-newport-beach-5"
+  },
+  {
+    title: "Knotts Berry Farm",
+    location: {lat: 33.8831646643, lng: -117.8868517391},
+    yelpID: "knotts-berry-farm-buena-park"
+  },
+  {
+    title: "Disney Land",
+    location: {lat: 33.8430500, lng: -118.0016600},
+    yelpID: "disneyland-park-anaheim-7"
+  },
+  {
+    title: "Universal Studios",
+    location: {lat: 34.13811680000001 , lng: -118.35337830000003},
+    yelpID: "universal-studios-hollywood-universal-city"
+  }
+
+];
